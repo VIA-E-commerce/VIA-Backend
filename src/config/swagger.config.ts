fbdaae050510c685f2dd1dde254e24e230c9ef88
Kyeongho Yang <88yangkh@gmail.com>
2022-02-22 10:22:00 +0900

    :memo: Swagger JWT Bearer 토큰 인증 설정

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,8 +1,19 @@
 import { APP } from '@/constant';
 import { DocumentBuilder } from '@nestjs/swagger';
 
+const BEARER_AUTH_NAME = 'Access Token';
+
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('VIA - API')
   .setDescription('VIA Fashion E-Commerce API Docs')
   .setVersion(APP.VERSION)
+  .addBearerAuth(
+    {
+      type: 'http',
+      description:
+        'Http Request의 Authorization 헤더로 넘어올 JWT access 토큰을 넣어주세요',
+    },
+    BEARER_AUTH_NAME,
+  )
+  .addSecurityRequirements(BEARER_AUTH_NAME)
   .build();
