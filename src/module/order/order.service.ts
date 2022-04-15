d5f53c3ceb3f73192ce5f69b7d56c12a5a462005
Kyeongho Yang <88yangkh@gmail.com>
2022-04-16 04:38:04 +0900

    :necktie: 배송 준비중 단계에서 주문을 취소할 수 없도록 변경

--- src/module/order/order.service.ts
+++ src/module/order/order.service.ts
@@ -193,8 +193,7 @@ export class OrderService {
 
       if (
         order.status !== OrderStatus.AWAITING_PAYMENT &&
-        order.status !== OrderStatus.PAYMENT_ACCEPTED &&
-        order.status !== OrderStatus.AWAITING_SHIPMENT
+        order.status !== OrderStatus.PAYMENT_ACCEPTED
       ) {
         throw new HttpException(
           '취소할 수 없는 주문입니다.',
