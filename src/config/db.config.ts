f96bcfe6b47bc607eee0494927490827f1d02147
Kyeongho Yang <88yangkh@gmail.com>
2022-04-27 09:07:01 +0900

    :card_file_box: TypeORM DB 타입, charset 변경

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -8,14 +8,14 @@ export const dbConfig = registerAs(CONFIG.DB, () => {
   const isDevMode = process.env.NODE_ENV === CONFIG.NODE_ENV.DEVELOPMENT;
   return {
     namingStrategy: new SnakeNamingStrategy(),
-    type: 'mysql',
+    type: 'mariadb',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     entities: getMetadataArgsStorage().tables.map((tbl) => tbl.target),
-    charset: 'utf8mb4_unicode_ci',
+    charset: 'utf8mb4_general_ci',
     synchronize: isDevMode,
     logging: isDevMode,
   };
