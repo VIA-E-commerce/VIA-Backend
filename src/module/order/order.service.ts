e29f7596b33e66fea9667ccb01071613fccc8b4a
Kyeongho Yang <88yangkh@gmail.com>
2022-04-28 15:38:58 +0900

    :mute: [주문] 주문 취소 구현 시 사용한 로그 제거

--- src/module/order/order.service.ts
+++ src/module/order/order.service.ts
@@ -197,7 +197,6 @@ export class OrderService {
         const calcedSalesVolume = product.salesVolume - orderDetail.quantity;
         product.salesVolume = Math.max(0, calcedSalesVolume);
 
-        console.log(variant);
         await variantRepository.save(variant);
         await productRepository.save(product);
       });
